fb809f26e2345de6c21867f5e2ca24db50afc3b4
Atul Varma <varmaa@gmail.com>
2015-04-26 12:35:36 -0400

    first pass at an implementation of build-webfont.js.

--- bin/build-webfont.js
+++ bin/build-webfont.js
@@ -1,1 +1,19 @@
-console.log("TODO: Build the webfont!");
+var fs = require('fs');
+
+var webfontsGenerator = require('webfonts-generator');
+
+webfontsGenerator({
+  files: fs.readdirSync('svg').map(function(filename) {
+    return 'svg/' + filename;
+  }),
+  dest: 'font/',
+  fontName: 'privicons',
+  html: true,
+  templateOptions: {
+    classPrefix: 'pi-',
+    baseClass: 'pi'
+  }
+}, function(err) {
+  if (err) throw err;
+  console.log("Yay!");
+});
